mapbox/stylelint-processor-arbitrary-tags

commit 58620a4945f5759e627c9dc62c179945b21dabc1

Remove no-empty-source treatment; prepare 0.1.1

index.js
@@ -4,12 +4,6 @@ const execall = require('execall');
 const splitLines = require('split-lines');
 const reindent = require('./lib/reindent');
 
-const ignoredRules = new Set([
-  // We don't want to reject files just because they
-  // have no CSS
-  'no-empty-source',
-]);
-
 const sourceToLineMap = new Map();
 
 module.exports = function (options) {
@@ -53,8 +47,6 @@ module.exports = function (options) {
   function transformResult(result, filepath) {
     const extractedToSourceLineMap = sourceToLineMap.get(filepath);
     const newWarnings = result.warnings.reduce((memo, warning) => {
-      if (ignoredRules.has(warning.rule)) return memo;
-
       const warningSourceMap = extractedToSourceLineMap.get(warning.line);
       if (warning.line) {
         warning.line = warningSourceMap.line;

commit dd1b61d4bc2332af143172e2b9de8735cf2b1268

Fixes no-empty-source file parsing
58620a4945f5759e627c9dc62c179945b21dabc1 introduced a bug where one
couldn't run the linter when there's a empty file and a
`no-empty-source` rule.

index.js
@@ -47,13 +47,19 @@ module.exports = function (options) {
   function transformResult(result, filepath) {
     const extractedToSourceLineMap = sourceToLineMap.get(filepath);
     const newWarnings = result.warnings.reduce((memo, warning) => {
+      // This might end up rendering to null when there's no content on the
+      // file being parsed. It's likely that it has been flagged due a
+      // `no-empty-source` rule.
       const warningSourceMap = extractedToSourceLineMap.get(warning.line);
-      if (warning.line) {
+
+      if (warning.line && warningSourceMap) {
         warning.line = warningSourceMap.line;
       }
-      if (warning.column) {
+
+      if (warning.column && warningSourceMap) {
         warning.column = warning.column + warningSourceMap.indentColumns;
       }
+
       memo.push(warning);
       return memo;
     }, []);

test/test.js
@@ -202,3 +202,24 @@ test('vue', (t) => {
     t.end();
   }).catch(t.end);
 });
+
+test('empty files with no-empty-source rule should be parsed', (t) => {
+  const fixture = path.join(__dirname, './fixtures/empty-file.html');
+  stylelint.lint({
+    files: [fixture],
+    config: {
+      processors: [pathToProcessor],
+      rules: {
+        'no-empty-source': true,
+      },
+    },
+  }).then((data) => {
+    t.equal(data.results.length, 1, 'number of results');
+    const result = data.results[0];
+    t.equal(result.source, fixture, 'filename');
+    result.warnings.forEach((warning) => {
+      t.equal(warning.rule, 'no-empty-source');
+    });
+    t.end();
+  }).catch(t.threw);
+});